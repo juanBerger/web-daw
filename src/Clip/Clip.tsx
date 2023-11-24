2d8455fc64e17f0942ff1438c0c496e75c483071
juanBerger <jaboit@gmail.com>
2023-11-24 01:11:46 -0500

    move clips

--- src/Clip/Clip.tsx
+++ src/Clip/Clip.tsx
@@ -1,19 +1,40 @@
 
+import React from 'react';
+import { useState } from 'react';
 import './Clip.css'
 
 
 export default function Clip(){
 
+    const [isClicked, setIsClicked] = useState(false);
+
+
     const handleClick = () =>{
-        //const cs = getComputedStyle();
-        //const pos = 
+        setIsClicked(!isClicked);
+    }
+
+    const handleMouseMove = (e: React.MouseEvent) => {
+
+        if (isClicked){
+
+            console.log(e.clientX);
+            const elem = document.querySelector('.c-parent') as HTMLElement;
+            const pos = Number(elem.style.getPropertyValue('--x-translate')) + e.clientX; 
+            elem.style.setProperty('--x-translate', String(pos) + 'px');
+            console.log(elem.style.getPropertyValue('--x-translate'));
+            
+        }
+
+        
+
+
 
     }
 
 
 
     return (
-        <div id='c-parent' onClick={handleClick}/>
+        <div className='c-parent' id='123' onClick={handleClick} onMouseMove={handleMouseMove}/>
     )
 
 
